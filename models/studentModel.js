15d1d839cb71dfeb8421ec21e1b77b6d49168389
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-11-15 20:33:04 +0530

    Update studentModel.js

--- models/studentModel.js
+++ models/studentModel.js
@@ -4,6 +4,20 @@ const studentSchema = new mongoose.Schema({
   // Field 1: Define a property for 'name' with a String data type and required true
   // Field 2: Define a property for 'age' with a Number data type and required true
   // Field 3: Define a property for 'score' with a Number data type and required true
+  
+   name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  score: {
+    type: Number, 
+    required: true
+  },
+  
 });
 
 const Student = mongoose.model('Student', studentSchema);
